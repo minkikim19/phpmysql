85a4f21fc89e352ffc931062376067b81fbc8031
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-08-13 04:20:47 +0900

    PHP와 MySQL의 연동과 INSERT

---
+++ htdocs/insert.php
@@ -0,0 +1,19 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "111111", "opentutorials");
+$sql = "
+        INSERT INTO topic (
+            title,
+            description,
+            created)
+        VALUES (
+            'MySQL',
+            'MySQL is ....',
+            NOW())
+        ";
+        echo $sql;
+$result = mysqli_query($conn, $sql);
+if($result == false)
+{
+    echo mysqli_error($conn);
+}
+?>
