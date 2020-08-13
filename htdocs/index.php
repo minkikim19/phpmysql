173679c98a53ad42e33fd009ff7007c08fc1558c
minkikim19 <66135779+minkikim19@users.noreply.github.com>
2020-08-14 06:15:26 +0900

    Add files via upload

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,14 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', '111111', 'opentutorials');
+
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+$list = '';
+while ($row = mysqli_fetch_array($result))
+{
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+}
+?>
 <!doctype html>
 <html>
   <head>
@@ -6,9 +17,7 @@
   </head>
   <body>
     <h1>WEB</h1>
-    <ol>
-      <li>HTML</li>
-    </ol>
+    <ol><?=$list?></ol>
     <a href = "create.php">create</a>
     <h2>Welcome</h2>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit
